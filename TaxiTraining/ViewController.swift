4c89ce5ebc56600a5f0193975510523e79283c27
Mogew <alimovren@gmail.com>
2023-09-12 20:43:44 +0600

    Initial Commit

---
+++ TaxiTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaxiTraining
+//
+//  Created by Renat on 12.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
